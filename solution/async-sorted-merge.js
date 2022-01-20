2506b3ce427efc8b15e647bb8b4ca01a162aa199
Nathan Kane <kanenathan213@gmail.com>
2022-01-19 16:01:55 -0800

    add nathan solutions

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,49 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+
+function insertIntoSortedPosition(item, arr) {
+  arr.splice(findInsertPosition(item, arr), 0, item)
+}
+
+function findInsertPosition(item, arr) {
+  let low = 0,
+      high = arr.length
+
+  while (low < high) {
+      const mid = Math.floor((low + high) / 2)
+      if (arr[mid].log.date < item.log.date) low = mid + 1
+      else high = mid
+  }
+  return low
+}
+
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    Promise.all(logSources.map(source => source.popAsync())).then((logs) => {
+      const sortedLogs = []
+      for (let i = 0; i < logs.length; i++) {
+        insertIntoSortedPosition({
+          log: logs[i],
+          logSourceIndex: i,
+        }, sortedLogs)
+      }
+      return sortedLogs
+    }).then((sortedLogs) => {
+      while (true) {
+        if (sortedLogs.length === 0) {
+          printer.done()
+          resolve(console.log("Async sort complete."))
+          break;
+        }
+        const { log: oldestLog, logSourceIndex } = sortedLogs.shift()
+        printer.print(oldestLog)
+        logSources[logSourceIndex].popAsync().then((nextLog) => {
+          if (nextLog !== false) {
+            insertIntoSortedPosition({ log: nextLog, logSourceIndex }, sortedLogs)
+          }
+        })
+      }
+    })
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,63 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+/*
+
+Possible strategies considered
+1) Throw all entries into a heap (essentially, let the heap sort them), then pop them off the heap and print them
+Pros - clean, easy
+Cons - allocates memory for all logs at once
+
+2) Find start and end logs for each log source. Sort by start
+Cons - we don't know end date, only have access to last log
+
+3) Pair log sources one by one, merge them
+
+4) (Chosen approach) Add all first entries to a sorted list, then shift / remove 0th index item, print it, then grab next item from that log source, insert it, repeat until empty
+
+Time: O(M*log N)
+Space: O(N) where N is number of log sources
+where N is number of log sources and M is length of longest log source
+
+*/
+
+function insertIntoSortedPosition(item, arr) {
+  arr.splice(findInsertPosition(item, arr), 0, item)
+}
+
+function findInsertPosition(item, arr) {
+  let low = 0,
+      high = arr.length
+
+  while (low < high) {
+      const mid = Math.floor((low + high) / 2)
+      if (arr[mid].log.date < item.log.date) low = mid + 1
+      else high = mid
+  }
+  return low
+}
+
 module.exports = (logSources, printer) => {
+  const currentItems = []
+  for (let i = 0; i < logSources.length; i++) {
+    const log = logSources[i].pop()
+    if (log === false) {
+      continue
+    }
+    insertIntoSortedPosition({
+      log,
+      logSourceIndex: i,
+    }, currentItems)
+  }
+  while (currentItems.length > 0) {
+    const { log: oldestLog, logSourceIndex } = currentItems.shift()
+    printer.print(oldestLog)
+    const nextLog = logSources[logSourceIndex].pop()
+    if (nextLog !== false) {
+      insertIntoSortedPosition({ log: nextLog, logSourceIndex }, currentItems)
+    }
+  }
+  
+  printer.done()
   return console.log("Sync sort complete.");
 };
